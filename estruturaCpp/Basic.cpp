678d2d9cfc646ea9c060e1f5b6bb42985e3a3998
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-12-07 18:25:00 -0300

    linux

---
+++ estruturaCpp/Basic.cpp
@@ -0,0 +1,14 @@
+// USE (#include) TO ADD LIBRARIES
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // (endl OR \n)  IS USED FOR LINE BREAKS
+    cout << "Hello World!\nCFB CURSOS" << endl;
+
+    system("pause");
+    return 0;
+}
