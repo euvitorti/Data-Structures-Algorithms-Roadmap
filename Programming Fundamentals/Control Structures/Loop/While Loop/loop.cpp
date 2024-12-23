3d00a127d8bcbca4517a78c19308d5d57725432c
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-12-23 15:24:32 -0300

    While Loop

---
+++ Programming Fundamentals/Control Structures/Loop/While Loop/loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i = 0; // Declare an integer variable 'i' and initialize it to 0.
+
+    // Start of the while loop. It will continue as long as 'i' is less than 5.
+    while (i < 5) {
+        cout << i << "\n"; // Print the current value of 'i' followed by a newline.
+        i++; // Increment 'i' by 1 in each iteration.
+    }
+
+    return 0; // Return 0 to indicate successful program execution.
+}
