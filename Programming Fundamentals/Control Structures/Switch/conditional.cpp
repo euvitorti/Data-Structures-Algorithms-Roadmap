96e1f6a3a629ae26dfb84086d8554a004dd4872a
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-12-23 15:15:56 -0300

    Switch

--- Programming Fundamentals/Control Structures/Switch/conditional.cpp
+++ Programming Fundamentals/Control Structures/Switch/conditional.cpp
@@ -4,36 +4,38 @@ using namespace std;
 
 int main(){
 
-    int day = 0;
+    int day = 0; // Declare an integer variable to store the day.
 
-    cout << "Enter the day: ";
+    cout << "Enter the day: "; // Prompt the user to enter a day number.
 
-    cin >> day; 
+    cin >> day; // Read the user input for the day number.
     
+    // Start of the switch statement to handle different cases based on the value of 'day'.
     switch (day) {
-    case 1:
+    case 1: // If 'day' is 1, print "Monday".
         cout << "Monday\n";
-    break;
-    case 2:
+        break; // Exit the switch statement after printing the result for this case.
+    case 2: // If 'day' is 2, print "Tuesday".
         cout << "Tuesday\n";
-    break;
-    case 3:
+        break; // Exit the switch statement after printing the result for this case.
+    case 3: // If 'day' is 3, print "Wednesday".
         cout << "Wednesday\n";
-    break;
-    case 4:
+        break; // Exit the switch statement after printing the result for this case.
+    case 4: // If 'day' is 4, print "Thursday".
         cout << "Thursday\n";
-    break;
-    case 5:
+        break; // Exit the switch statement after printing the result for this case.
+    case 5: // If 'day' is 5, print "Friday".
         cout << "Friday\n";
-    break;
-    case 6:
+        break; // Exit the switch statement after printing the result for this case.
+    case 6: // If 'day' is 6, print "Saturday".
         cout << "Saturday\n";
-    break;
-    case 7:
+        break; // Exit the switch statement after printing the result for this case.
+    case 7: // If 'day' is 7, print "Sunday".
         cout << "Sunday\n";
-    default:
+        break; // Exit the switch statement after printing the result for this case.
+    default: // If 'day' doesn't match any case (1-7), print "Day invalid".
         cout << "Day invalid\n";
-        break;
+        break; // Exit the switch statement after printing the error message.
     }
 
     return 0; // Return 0 to indicate successful program execution.
