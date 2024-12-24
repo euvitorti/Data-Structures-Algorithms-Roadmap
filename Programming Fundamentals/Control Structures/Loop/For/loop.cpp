56c84dc44b8ec625c19ef2e8daf8315a24a183c1
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-12-24 14:33:24 -0300

    C++ For Loop

---
+++ Programming Fundamentals/Control Structures/Loop/For/loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Include the library for input and output.
+
+using namespace std; // Use the standard namespace to simplify code syntax.
+
+int main() {
+
+    // The `for` loop is an alternative to the `do-while` loop.
+    // It is typically used when the number of iterations is known beforehand.
+    for (int i = 0; i < 5; i++) { // Initialize `i` to 0; run the loop while `i` is less than 5; increment `i` by 1 after each iteration.
+        cout << i << "\n"; // Print the current value of `i` followed by a newline.
+    }
+
+    return 0; // Return 0 to indicate successful program execution.
+}
